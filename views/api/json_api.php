e8a0a4cc4225536098795f538a66b1ecf1e73369
Julian Seidl <julian@julianseidl.at>
2019-06-26 12:45:39 +0200

    Added GitHub hosted Quotes

--- views/api/json_api.php
+++ views/api/json_api.php
@@ -4,7 +4,7 @@
         die();
     }
 
-    $quotesFile = file_get_contents('http://scooterquotes.loc/scooter-quotes.json');
+    $quotesFile = file_get_contents('https://raw.githubusercontent.com/Thejuse/scooterquotes/quotes/scooter-quotes.json');
     $quotesArray = json_decode($quotesFile, true);
 
     $JSON = json_encode($quotesArray);
